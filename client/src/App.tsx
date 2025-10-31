2f18aa101561b32e5d9e68aad3c6c1d3819a09b9
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-10-31 21:24:32 +0500

    Add nav bar + Change font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,37 @@
 import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/floating-navbar";
+
+const navItems = [
+  {
+    name: "ABOUT",
+    link: "/about",
+  },
+  {
+    name: "PROJECTS",
+    link: "/projects",
+  },
+  {
+    name: "SKILLS",
+    link: "/skills",
+  },
+  {
+    name: "CONTACT",
+    link: "/contact",
+  },
+];
 
 function App() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero />
-      </div>
-    </main>
+    <>
+      <nav>
+        <FloatingNav navItems={navItems} />
+      </nav>
+      <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <Hero />
+        </div>
+      </main>
+    </>
   );
 }
 
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -10,15 +10,16 @@ const Hero = () => {
     <div className="pb-20 pt-36">
       <Spotlight />
       <div className="flex flex-col items-center justify-center p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-linear-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl md:text-7xl font-semibold text-center bg-clip-text text-transparent bg-linear-to-b from-neutral-200 to-neutral-400 bg-opacity-50">
           Transforming Ideas into Seamless User Experiences
         </h1>
         <TextGenerateEffect
           words={words}
-          className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto"
+          className="mt-4 font-normal text-base max-w-lg text-center mx-auto"
         />
         <Button position="right" title="My work" icon={<FaLocationArrow />} />
       </div>
+      <Spotlight />
     </div>
   );
 };
