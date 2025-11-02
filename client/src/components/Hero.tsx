29bde088ea227792a0c2308902cd15e5815b521c
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-02 19:16:57 +0500

    Make ui

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <Spotlight />
       <Spotlight />
       <div className="flex flex-col items-center justify-center p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-semibold text-center bg-clip-text text-transparent bg-linear-to-b from-neutral-200 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl md:text-7xl font-semibold text-center bg-clip-text text-transparent bg-linear-to-b from-textColor to-secondaryColor bg-opacity-50">
           Transforming Ideas into Seamless User Experiences
         </h1>
         <TextGenerateEffect
--- client/src/components/ui/floating-navbar.tsx
+++ client/src/components/ui/floating-navbar.tsx
@@ -55,7 +55,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border-2 border-white/40 rounded-2xl dark:bg-black bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-5000 px-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border-2 border-secondaryColor rounded-2xl bg-primaryColor shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-5000 px-8 py-2 items-center justify-center space-x-4",
           className
         )}
       >
@@ -64,7 +64,7 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-200 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative items-center flex space-x-1 text-textColor hover:text-accentColor"
             )}
           >
             <span className="text-sm sm:block sm:text-md">{navItem.name}</span>
