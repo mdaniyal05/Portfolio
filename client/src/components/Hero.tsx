f957b940b9f8d7feab7f2b73887a0df6f2d441e1
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-02 23:01:11 +0500

    Fix

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -7,7 +7,7 @@ const words = "Hi! I'm Daniyal, a Full Stack Web Developer based in Pakistan";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36 h-screen flex justify-center items-center">
+    <div className="pb-20 pt-36 flex justify-center items-center">
       <Spotlight />
       <Spotlight />
       <div className="flex flex-col items-center justify-center p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
