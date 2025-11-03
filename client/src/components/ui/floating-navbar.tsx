4fe5bd13ef6d8c70f4660d13f993c1005d6fe2ec
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-03 12:09:36 +0500

    Fix

--- client/src/components/ui/floating-navbar.tsx
+++ client/src/components/ui/floating-navbar.tsx
@@ -46,7 +46,7 @@ export const FloatingNav = ({
   };
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="sync">
       <motion.div
         initial={{
           opacity: 1,
@@ -57,7 +57,7 @@ export const FloatingNav = ({
           opacity: visible ? 1 : 0,
         }}
         transition={{
-          duration: 0.2,
+          duration: 0.6,
         }}
         className={cn(
           "flex max-w-fit fixed top-10 inset-x-0 mx-auto border-2 border-secondaryColor rounded-2xl bg-primaryColor shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-5000 px-8 py-2 items-center justify-center space-x-4 w-75 md:w-auto",
