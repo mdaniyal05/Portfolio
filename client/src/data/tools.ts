18d7287e8ff705aa21c0deb47928557ada5bd89e
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-08 09:57:26 +0500

    Separate data

---
+++ client/src/data/tools.ts
@@ -0,0 +1,105 @@
+export const tools1 = [
+  {
+    svg: "/js.svg",
+    name: "JavaScript",
+  },
+  {
+    svg: "/ts.svg",
+    name: "TypeScript",
+  },
+  {
+    svg: "/nodejs.svg",
+    name: "Node JS",
+  },
+  {
+    svg: "/react.svg",
+    name: "React",
+  },
+  {
+    svg: "/mongodb.svg",
+    name: "Mongo DB",
+  },
+  {
+    svg: "/express.svg",
+    name: "Express JS",
+  },
+  {
+    svg: "/linux.svg",
+    name: "Linux",
+  },
+  {
+    svg: "/arch.svg",
+    name: "Arch Linux",
+  },
+  {
+    svg: "/api.svg",
+    name: "REST API",
+  },
+  {
+    svg: "/css.svg",
+    name: "CSS",
+  },
+  {
+    svg: "/git.svg",
+    name: "Git",
+  },
+  {
+    svg: "/github.svg",
+    name: "Github",
+  },
+];
+
+export const tools2 = [
+  {
+    svg: "/html.svg",
+    name: "HTML",
+  },
+  {
+    svg: "/materialui.svg",
+    name: "Material UI",
+  },
+  {
+    svg: "/postgresql.svg",
+    name: "PostgreSQL",
+  },
+  {
+    svg: "/redux.svg",
+    name: "Redux Toolkit",
+  },
+  {
+    svg: "/npm.svg",
+    name: "NPM",
+  },
+  {
+    svg: "/tailwind.svg",
+    name: "Tailwind CSS",
+  },
+  {
+    svg: "/java.svg",
+    name: "Java",
+  },
+  {
+    svg: "/mongoose.svg",
+    name: "Mongoose",
+  },
+  {
+    svg: "/motion.svg",
+    name: "Framer Motion",
+  },
+  {
+    svg: "/nextjs.svg",
+    name: "Next JS",
+  },
+  {
+    svg: "/python.svg",
+    name: "Python",
+  },
+  {
+    svg: "/sequelize.svg",
+    name: "Sequelize",
+  },
+  {
+    svg: "/bash.svg",
+    name: "Bash",
+  },
+];
