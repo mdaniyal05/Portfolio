ec6e4889156abac3c1915e185172898f18641992
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-08 09:31:01 +0500

    Separate exports for fast refresh

--- client/src/components/ui/globe.tsx
+++ client/src/components/ui/globe.tsx
@@ -5,6 +5,7 @@ import ThreeGlobe from "three-globe";
 import { useThree, Canvas, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import countries from "@/data/globe.json";
+import { genRandomNumbers } from "@/hooks/useGenRandomNumbers";
 
 declare module "@react-three/fiber" {
   interface ThreeElements {
@@ -278,32 +279,4 @@ export function World(props: WorldProps) {
       />
     </Canvas>
   );
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-export function hexToRgb(hex: string) {
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function (_m, r, g, b) {
-    return r + r + g + g + b + b;
-  });
-
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-export function genRandomNumbers(min: number, max: number, count: number) {
-  const arr = [];
-  while (arr.length < count) {
-    const r = Math.floor(Math.random() * (max - min)) + min;
-    if (arr.indexOf(r) === -1) arr.push(r);
-  }
-
-  return arr;
 }
