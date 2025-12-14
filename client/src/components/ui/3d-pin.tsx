bc6afacf5bd615f77b8da27bf90af80e184e9745
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-12-14 22:29:05 +0500

    Fix

--- client/src/components/ui/3d-pin.tsx
+++ client/src/components/ui/3d-pin.tsx
@@ -72,7 +72,7 @@ export const PinPerspective = ({
           target="_blank"
         >
           <div className="relative flex space-x-2 items-center z-10 rounded-full bg-secondaryColor py-0.5 px-4 ring-1 ring-white/10 ">
-            <span className="relative z-20 text-primaryColor text-xs font-bold inline-block py-0.5">
+            <span className="relative text-center z-20 text-primaryColor text-xs font-bold inline-block py-0.5">
               {title}
             </span>
 
