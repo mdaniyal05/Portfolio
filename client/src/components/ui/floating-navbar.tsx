3609f31ae55313ff1caabd6e01a9d198cb46fb26
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-12-25 11:17:15 +0500

    Fix

--- client/src/components/ui/floating-navbar.tsx
+++ client/src/components/ui/floating-navbar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState, type JSX } from "react";
+import { useState, type JSX } from "react";
 import {
   motion,
   AnimatePresence,
